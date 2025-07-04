77c1754b16e90ca4e5587bada7df680923dc5b72
Mohak Jaiswal <mohakjaiswal@Mac.lan>
2025-07-05 01:24:40 +0530

    Added pages using ejs

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express();
 const db = require('./models/db')
+const path = require('path')
+
+app.set("view engine","ejs")
+app.use(express.json());
+app.use(express.urlencoded({extended : true}))
+app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/',(req,res)=>{
-    console.log()
+    res.render("index")
 })
-
+app.get('/get',(req,res)=>{
+    res.render("read")
+})
 app.post('/create-user',(req,res)=>{
     const {name,email,image} = req.body;
 })
@@ -14,7 +22,7 @@ app.delete('/delete:id',(req,res)=>{
 
 })
 
-app.update('/',(req,res)=>{
+app.post('/',(req,res)=>{
 
 })
 
--- models/db.js
+++ models/db.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(MONGOOSE_URL);
+const dotenv = require('dotenv');
+dotenv.config()
+const MU = process.env.MONGOOSE_URL;
+mongoose.connect(MU)
+.then(()=>{
+    console.log("mongodb connnected successfully")
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 charSchema = mongoose.Schema({
     text : String
 })
+
+module.exports = mongoose.model('chars',charSchema)
